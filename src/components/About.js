d5101351beada740817b55636ff249fb10a6f29f
Todd Dukart <tdukart@aloftinteractive.com>
2017-08-25 09:46:08 -0400

    Update About screen and manifest

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import packageProps from '../../package.json';
 
 export default class About extends React.Component {
 	render() {
 		return (
 			<div className="container">
-				<h2>WordFinder</h2>
+				<h2>WordFinder {packageProps.version}</h2>
 				<p>Developed by Todd Dukart</p>
 				<p><a href="https://github.com/tdukart/wordfinder">GitHub</a></p>
 			</div>
